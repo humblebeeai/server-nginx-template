f437ff535dc0a522943e431ee3b9dd7433a544a2
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-10-02 12:07:31 +0900

    fix: update pull command to use 'always' policy in _doUpdate function

--- compose.sh
+++ compose.sh
@@ -156,7 +156,7 @@ _doUpdate()
 	fi
 
 	# shellcheck disable=SC2068
-	docker compose pull ${@:-} || exit 2
+	docker compose pull --policy always ${@:-} || exit 2
 	# shellcheck disable=SC2046
 	docker rmi -f $(docker images --filter "dangling=true" -q --no-trunc) > /dev/null 2>&1 || true
 
